f22d8e857cf382035ccac4765c3919db48818ad1
bommireddy30 <durgareddybommireddy1901@gmail.com>
2022-05-27 00:50:46 +0530

    Commit

---
+++ calc/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>This is First Project</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
