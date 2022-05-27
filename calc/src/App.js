501e0c566ef458689ff7076955a06f293eaaa11c
bommireddy30 <durgareddybommireddy1901@gmail.com>
2022-05-27 12:39:43 +0530

    We have created Wrapper for Calculator

--- calc/src/App.js
+++ calc/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Wrapper />
     </div>
   );
 }
