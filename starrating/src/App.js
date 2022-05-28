13151f8fef646c88cca9d2ffca3530958a386eee
bommireddy30 <durgareddybommireddy1901@gmail.com>
2022-05-29 02:40:33 +0530

    Started a new project/StarRating

---
+++ starrating/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>We are going to create Star Rating</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
