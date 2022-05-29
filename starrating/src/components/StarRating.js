119710a461e5af98e0c6fca6886b0ebb9b37fa6b
bommireddy30 <durgareddybommireddy1901@gmail.com>
2022-05-29 12:35:25 +0530

    Completed the starRating/StarRating

--- starrating/src/components/StarRating.js
+++ starrating/src/components/StarRating.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import classes from "./StarRating.module.css";
 function StarRating() {
   const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
   useEffect(() => {
     console.log(rating);
   }, [rating]);
@@ -12,9 +13,11 @@ function StarRating() {
         return (
           <button
             type="button"
-            className={index <= rating ? classes.on : classes.off}
+            className={index <= (hover || rating) ? classes.on : classes.off}
             key={index}
             onClick={() => setRating(index)}
+            onMouseEnter={() => setHover(index)}
+            onMouseLeave={() => setHover(rating)}
           >
             <span className="star">&#9733;</span>
           </button>
