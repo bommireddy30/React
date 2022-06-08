734414ab51df4172484235ea8c51512dffb552c7
bommireddy30 <durgareddybommireddy1901@gmail.com>
2022-06-09 03:34:49 +0530

    Checking few changes

--- bigone/src/Components/LoginModal.js
+++ bigone/src/Components/LoginModal.js
@@ -6,12 +6,14 @@ function LoginModal() {
   return ReactDom.createPortal(
     <Card>
       <form className="loginForm">
-        <label>Email</label>
-        <input type="email"></input>
-        <label>PassWord</label>
-        <input type="password"></input>
-        <div>Forgot PassWord??</div>
-        <button>Login</button>
+        <label className="label">Email</label>
+        <input type="email" className="loginForm"></input>
+        <label className="label">PassWord</label>
+        <input type="password" className="loginForm"></input>
+        <a href="#" className="forgot loginForm">
+          Forgot PassWord??
+        </a>
+        <button className="btn--modal">Login</button>
       </form>
     </Card>,
     document.getElementById("modal")
