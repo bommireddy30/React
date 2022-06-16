495814e444a6750886027add250dd04174eb3dc1
bommireddy30 <durgareddybommireddy1901@gmail.com>
2022-06-16 12:53:51 +0530

    Implementing Redux/Todo

--- todo-list/src/Components/TodoList.js
+++ todo-list/src/Components/TodoList.js
@@ -2,15 +2,14 @@ import classes from "../Components/Styles/TodoList.module.css";
 import Card from "./Card";
 function TodoList(props) {
   return props.todoList.map((todoTask) => (
-    <div
-      className={classes.listItem}
-      key={todoTask.id}
-      onClick={(e) => {
-        console.log(todoTask.id);
-        return props.deleteTask(todoTask.id);
-      }}
-    >
-      <input type="checkbox" className={classes.check} />
+    <div className={classes.listItem} key={todoTask.id}>
+      <input
+        type="checkbox"
+        className={classes.check}
+        onChange={(e) => {
+          props.deleteTask(todoTask.id);
+        }}
+      />
       <li>{todoTask.task}</li>
     </div>
   ));
