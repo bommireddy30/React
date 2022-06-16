a399eaaed19217c31397d034aac36bd32140acc4
bommireddy30 <durgareddybommireddy1901@gmail.com>
2022-06-16 12:49:52 +0530

    We are just a step behind to transfer the code to Redux/Todo

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -5,14 +5,17 @@ import TodoList from "./Components/TodoList";
 
 function App() {
   const [todo, setTodo] = useState([]);
+  const [key, setKey] = useState(0);
   const inputTodo = useRef();
 
   const addTodoHandler = () => {
-    setTodo([
-      ...todo,
-      { task: inputTodo.current.value, id: Math.floor(Math.random() * 20) },
-    ]);
+    setKey((key) => key + 1);
+    setTodo([...todo, { task: inputTodo.current.value, id: key }]);
     inputTodo.current.value = "";
+  };
+
+  const deleteHandler = (key) => {
+    return setTodo((prevState) => prevState.filter((task) => task.id !== key));
   };
   return (
     <div className="todo--list">
@@ -28,7 +31,7 @@ function App() {
         </button>
       </Card>
       <div className="todoList">
-        <TodoList todoList={todo} key={todo} />
+        <TodoList todoList={todo} key={todo} deleteTask={deleteHandler} />
       </div>
     </div>
   );
--- todo-list/src/Components/TodoList.js
+++ todo-list/src/Components/TodoList.js
@@ -2,9 +2,16 @@ import classes from "../Components/Styles/TodoList.module.css";
 import Card from "./Card";
 function TodoList(props) {
   return props.todoList.map((todoTask) => (
-    <div className={classes.listItem}>
+    <div
+      className={classes.listItem}
+      key={todoTask.id}
+      onClick={(e) => {
+        console.log(todoTask.id);
+        return props.deleteTask(todoTask.id);
+      }}
+    >
       <input type="checkbox" className={classes.check} />
-      <li key={todoTask.id}>{todoTask.task}</li>
+      <li>{todoTask.task}</li>
     </div>
   ));
 }
